2cdfe265311b52a03d11ce89f9a4d580ca51d986
honism <52316339+honism@users.noreply.github.com>
2019-06-29 19:02:00 +0700

    Create Exercise_4_Thanaphol_C.py

---
+++ Exercise_4_Thanaphol_C.py
@@ -0,0 +1,9 @@
+Eng = 78.5
+Bus = 84
+Com_Sys = 81
+Com_Pro = 82.5
+print("---Your Score---")
+print("Foundation English :",Eng)
+print("General Business:",Bus)
+print("Introduction to Computer Systems:",Com_Sys)
+print("Computer Programming:",Com_Pro)
