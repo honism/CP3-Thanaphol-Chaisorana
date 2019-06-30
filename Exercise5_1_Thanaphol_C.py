21daa574659ec2a1049bad1d2eeec6d1fda25be8
honism <52316339+honism@users.noreply.github.com>
2019-06-30 19:21:09 +0700

    Create Exercise5_1_Thanaphol_C.py

---
+++ Exercise5_1_Thanaphol_C.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter Number1:"))
+num2 = int(input("Enter Number2:"))
+print("%d + %d = %d"%(num1,num2,num1+num2))
+print("%d - %d = %d"%(num1,num2,num1-num2))
+print("%d * %d = %d"%(num1,num2,num1*num2))
+print("%d / %d = %d"%(num1,num2,num1/num2))
