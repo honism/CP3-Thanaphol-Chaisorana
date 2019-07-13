286f28349e6e359956e6ac7e94bd32ea7525a6fd
honism <52316339+honism@users.noreply.github.com>
2019-07-13 18:40:26 +0700

    Create Lecture50_Thanaphol_C.py

---
+++ Lecture50_Thanaphol_C.py
@@ -0,0 +1,17 @@
+def addnumber(x,y):
+    print(x+y)
+def minus(x,y):
+    print(x-y)
+def multiplie(x,y):
+    print(x*y)
+def divide(x,y):
+    print(x//y)
+
+print("addnumber")
+addnumber(float(input("num1:")),float(input("num2:")))
+print("minusnumber")
+minus(float(input("num1:")),float(input("num2:")))
+print("multiplienumber")
+multiplie(float(input("num1:")),float(input("num2:")))
+print("dividenumber")
+divide(float(input("num1:")),float(input("num2:")))
